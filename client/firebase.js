fbfdbc555ed286ecef73533307b22b4181915efc
CharlesExists <charlesampah02@gmail.com>
2025-11-04 18:36:30 -0500

    Remove files that shouldnt be tracked, hopefully we good

---
+++ client/firebase.js
@@ -0,0 +1,16 @@
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync("./config/serviceAccountKey.json", "utf8")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export const auth = admin.auth();
+export const db = admin.firestore();
+
