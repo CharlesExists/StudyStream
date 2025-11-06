d66ec1d1224ee639212a78e32a875c921819995c
jmarla93 <janellmarla.m@gmail.com>
2025-11-06 16:41:27 -0500

    merging main

--- client/src/components/SoloStudyStart.jsx
+++ client/src/components/SoloStudyStart.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./SoloStudyStart.css";
 import blueLogo from "../assets/blueStudyStreamLogo.png";
 import Boat from "../assets/boat.png";
+import { Link, NavLink } from "react-router-dom";
 
 export default function SoloStudyStart({ username, defaultBoat }) {
   const [boat, setBoat] = useState(defaultBoat);
@@ -11,19 +12,16 @@ export default function SoloStudyStart({ username, defaultBoat }) {
       <header className="brand solostart-logo">
         <img src={blueLogo} alt="Blue StudyStream Logo" className="logo-mark" />
         <span className="brand-text">StudyStream</span>
+        <Link to="/Home" className ="home-btn">Home</Link>
       </header>
 
       <div className="water"></div>
 
-      <img src={boat} alt={`${username}'s boat`} className="boat-avatar" />
+      <img src={Boat} alt={`${username}'s boat`} className="boat-avatar-start" />
 
       <div className="study-options">
         <h1 className="study-title">Solo Study Start</h1>
       </div>
-
-      <button className="home-btn" onClick={() => window.location.href = "/"}>
-        Home
-      </button>
     </div>
   );
 }
