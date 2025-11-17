dce837586b293d317191e84deff5d2a11f2e42e1
Sarah Toussaint <sat10002@nyu.edu>
2025-11-17 14:40:39 -0500

    Invite friends

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home";
 import SoloStudyStart from './components/SoloStudyStart';
 import Materials from "./components/Materials";
 import CreateNotesScreen from "./components/CreateNotesScreen.jsx";
+import InviteFriendsStart from "./components/InviteFriendsStart.jsx";
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
           <Route path="/" element={<Navigate to="/Materials" replace />} />
           <Route path="/Materials" element={<Materials />}></Route>
           <Route path="/materials/create" element={<CreateNotesScreen />} />
+          <Route path="/invite" element={<InviteFriendsStart />} />
         </Route>
       </Routes>
     </div>
